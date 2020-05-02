c646c16018c71574188b1b56215a24e72f92df81
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-05-02 12:23:32 -0500

    input student names

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log('This is the Sorting Hats exercise!');
+/*save user input from form to push into an array*/
+let students = [];
+
+const addStudent = (event) => {
+  let student = {
+    name: document.getElementById('studentName').value
+  }
+  students.push(student);
+  document.forms[0].reset(); /*clear form for next name*/
+  console.log(student)
+}
+
+const clickEvent = () => {
+  document.querySelector('#sort-btn').addEventListener('click', addStudent);
+};
+
+const init = () => {
+  clickEvent();
+}
+
+init();
+
+
+/*const houses = [
+  {
+    student:'',
+    type: Gryffindor
+  },
+  {
+    student:'',
+    type: Ravenclaw
+  },
+  {
+    student:'',
+    type: Slytherin
+  },
+  {
+    student:'',
+    type: Hufflepuff
+  }
+],
+
+const printToDom = (selector, text*/
