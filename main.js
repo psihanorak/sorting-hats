ddc852ae669c5f29dd31c9d12e0dba4fa0295875
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-05-02 14:34:24 -0500

    domString added for house cards

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*save user input from form to push into an array*/
-let students = [];
+var students = [];
 
 const addStudent = (event) => {
   let student = {
@@ -10,34 +10,70 @@ const addStudent = (event) => {
   console.log(student)
 }
 
+/*create array for houses*/
+const house = [
+  {
+    name: '${students}',
+    type: 'Gryffindor'
+  },
+  {
+    name: '${students}',
+    type: 'Ravenclaw'
+  },
+  {
+    name: '${students}',
+    type: 'Slytherin'
+  },
+  {
+    name: '${students}',
+    type: 'Hufflepuff'
+  },
+  {
+    name: '${students}',
+    type: 'Gryffindor'
+  },
+  {
+    name: '${students}',
+    type: 'Ravenclaw'
+  },
+  {
+    name: '${students}',
+    type: 'Slytherin'
+  },
+  {
+    name: '${students}',
+    type: 'Hufflepuff'
+  }
+];
+
+const printToDom = (selector, textToPrint) => {
+  const selectedDiv = document.querySelector(selector);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const buildHouse = (array) => {
+  let domString = '';
+
+  for (let i = 0; i < array.length; i++) {
+    domString += `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${house[i].name}</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">${house[i].type}</h6>
+                      <button type="button" id="expel-btn" class="btn btn-primary btn-sm">Expel</button>
+                    </div>
+                  </div>`;
+  }
+
+  printToDom('#container-card', domString);
+}
+
 const clickEvent = () => {
   document.querySelector('#sort-btn').addEventListener('click', addStudent);
 };
 
 const init = () => {
+  buildHouse(house);
   clickEvent();
 }
 
 init();
-
-
-/*const houses = [
-  {
-    student:'',
-    type: Gryffindor
-  },
-  {
-    student:'',
-    type: Ravenclaw
-  },
-  {
-    student:'',
-    type: Slytherin
-  },
-  {
-    student:'',
-    type: Hufflepuff
-  }
-],
-
-const printToDom = (selector, text*/
