88359be3cd6533149c714fe1b89bc379b0a6a21a
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-05-07 22:57:38 -0500

    Student gets assigned random house

--- main.js
+++ main.js
@@ -1,70 +1,72 @@
-/*create alert window when no input is entered*/
-function warning() {
-  if (document.getElementById('studentName').value == "") {
-    alert('Please enter your name in order to be assigned a house!');
-    return false;  
+//empty student array and unique Id
+let studentArr = [];
+let uniqueId = 0;
+
+//array of schools
+const houses = ['Gryffindor','Ravenclaw', 'Slytherin', 'Hufflepuff'];
+
+
+//function to assign a random house to a student
+const randomHouse = () => {
+  const house = Math.floor(Math.random() * 4);
+  return houses[house];
+}
+
+//function that adds a student once it is passed through the input
+const addStudents = () => {
+  const studentName = document.getElementById('studentName').value;
+    studentArr.unshift(
+    {
+      name: studentName,
+      house: randomHouse(),
+      id: uniqueId
+    }
+    );
+
+    uniqueId++
+    buildCards(studentArr)
+}
+
+const enteredName = () => {
+  if (document.getElementById('studentName').value === "") {
+    alert('Please enter your name in order to be placed into a school!');
+  } else {
+    addStudents();
+    document.getElementById('studentName').value = "";
   }
 }
 
-/*create array for houses*/
-const house = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'];
-const sortStudent = [];
-
-const assignHouse = () => {
-  let random = Math.floor(Math.random() * (house.length));
-    document.getElementById('sort-btn').value = house[random];
-    sortStudent.push(document.getElementById('studentName').value, (house[random]));
-}
-
-/*sort students*/
-let students = [];
-let studentIndex = 0;
-
-const sortBtn = () => {
-  if (warning() == true) {
-    assignHouse ();
-    let newStudent = {};
-    newStudent ['name'] = document.getElementById('studentName').value;
-    newStudent ['house'] = house[Math.floor(Math.random() * (house.length))];
-    newStudent ['uniqueId'] = studentIndex;
-    console.log(newStudent);
-    students.push(newStudent);
-    console.log(students);
-    buildStudentCards(students);
-    document.getElementById('studentName').value = null;
-    studentIndex++
-  }
-
-}
-
-/*create dom for cards*/
+//print to domString
 const printToDom = (selector, textToPrint) => {
   const selectedDiv = document.querySelector(selector);
   selectedDiv.innerHTML = textToPrint;
 }
 
-const buildHouse = (students) => {
+const buildCards = (collection) => {
   let domString = '';
 
-  for (let i = 0; i < students.length; i++) {
-    domString += `<div class="card" style="width: 18rem;">
+  for (let i = 0; i < collection.length; i++) {
+    domString = `<div class="col-sm-6">
+                  <div class="card">
                     <div class="card-body">
-                      <h5 class="card-title">${students[i].name}</h5>
-                      <h6 class="card-subtitle mb-2 text-muted">${students[i].house}</h6>
-                      <button type="button" id=${students[i].uniqueId} class="btn btn-primary btn-sm">Expel</button>
+                      <h5 class="card-title">${collection[i].name}</h5>
+                      <p class="card-text">${collection[i].house}</p>
+                      <a href="#" class="btn btn-primary">Expel</a>
                     </div>
-                  </div>`;
+                  </div>
+                </div>`;
   }
 
   printToDom('#container-card', domString);
 }
 
+//click event when sorting button is pressed to sort students to random house and check to see if input field is not empty
 const clickEvent = () => {
-  document.querySelector('#sort-btn').addEventListener('click', sortBtn);
-};
+  document.querySelector('#startSort').addEventListener('click', enteredName);
+}
 
 const init = () => {
-  buildHouse
+  buildCards(studentArr);
   clickEvent();
 }
 
