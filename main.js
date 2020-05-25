1bd0862b2530562e9547d2689dab4cf2959a53a8
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-05-25 00:39:51 -0500

    students now applied to DOMcards

--- main.js
+++ main.js
@@ -1,73 +1,74 @@
-//empty student array and unique Id
-let studentArr = [];
-let uniqueId = 0;
+// Need to jump to the student form when "start sorting" button is clicked.
+document.getElementById('start-sort').addEventListener('click', function() {
+  document.getElementById('container-jumbotron').hidden = true;
+  document.getElementById('container-form').hidden = false;
+}, false);
 
-//array of schools
+// Empty array to store students.
+let students = [];
+let studentId = 0;
+
+// Array used to store the four houses.
 const houses = ['Gryffindor','Ravenclaw', 'Slytherin', 'Hufflepuff'];
 
-
-//function to assign a random house to a student
+// Generates a random house to assign the student once they submit their name in the form.
 const randomHouse = () => {
   const house = Math.floor(Math.random() * 4);
   return houses[house];
+  }
+
+// Create a new student once the input is captured and add into the student array and create student card.
+const createNewStudent = () => {
+  const name = document.getElementById('studentName').value;
+  
+  const newStudent = {
+    name: name,
+    house: randomHouse(),
+    id: studentId
+  };
+
+  studentId++
+  students.unshift(newStudent);
+  createStudentCards(students);
+};
+
+// Utility function created below.
+const printToDom = (selector, textToPrint) => {
+  document.querySelector(selector).innerHTML = textToPrint;
 }
 
-//function that adds a student once it is passed through the input
-const addStudents = () => {
-  const studentName = document.getElementById('studentName').value;
-    studentArr.unshift(
-    {
-      name: studentName,
-      house: randomHouse(),
-      id: uniqueId
-    }
-    );
-
-    uniqueId++
-    buildCards(studentArr)
-}
-
-const enteredName = () => {
-  if (document.getElementById('studentName').value === "") {
+// Sorts a student into a random house or notifies the user to enter their name.
+const sortStudent = () => {
+  if (document.getElementById('studentName').value === '') {
     alert('Please enter your name in order to be placed into a school!');
   } else {
-    addStudents();
-    document.getElementById('studentName').value = "";
+    createNewStudent();
+    document.getElementById('studentName').value = '';
   }
-}
+};
 
-//print to domString
-const printToDom = (selector, textToPrint) => {
-  const selectedDiv = document.querySelector(selector);
-  selectedDiv.innerHTML = textToPrint;
-}
-
-const buildCards = (collection) => {
+// DOM string to loop students into and build the cards.
+const createStudentCards = (studentCollection) => {
   let domString = '';
 
-  for (let i = 0; i < collection.length; i++) {
-    domString = `<div class="col-sm-6">
-                  <div class="card">
-                    <div class="card-body">
-                      <h5 class="card-title">${collection[i].name}</h5>
-                      <p class="card-text">${collection[i].house}</p>
-                      <a href="#" class="btn btn-primary">Expel</a>
-                    </div>
-                  </div>
-                </div>`;
-  }
+    for (let i = 0; i < studentCollection.length; i++) {
+      const student = studentCollection[i]
+      domString += `<div style="margin: 0px 30px 50px">
+                      <div class="card">
+                        <div class="card-body">
+                          <h5 class="card-title">${student.name}</h5>
+                          <p class="card-text">${student.house}</p>
+                          <a href="#" id="${student.id}" class="btn btn-primary">Expel</a>
+                        </div>
+                      </div>
+                    </div>`;
+    };
 
   printToDom('#container-card', domString);
-}
-
-//click event when sorting button is pressed to sort students to random house and check to see if input field is not empty
-const clickEvent = () => {
-  document.querySelector('#startSort').addEventListener('click', enteredName);
-}
+};
 
 const init = () => {
-  buildCards(studentArr);
-  clickEvent();
-}
+  document.getElementById('startSort').addEventListener('click', sortStudent);
+};
 
 init();
